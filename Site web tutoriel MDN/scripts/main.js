9b01f4450ce0859bc9ea889b50a822f45450a719
B1nch <128066348+B1nch@users.noreply.github.com>
2023-03-16 16:20:38 +0100

    Add files via upload

---
+++ Site web tutoriel MDN/scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/arbre.png') {
+      myImage.setAttribute('src', 'images/arbre2.png');
+    } else {
+      myImage.setAttribute('src', 'images/arbre.png');
+    }
+});
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Entrez votre nom');
+    localStorage.setItem('nom', myName);
+    myHeading.textContent = 'Bonjour, ' + myName;
+  }
+  
+  if (!localStorage.getItem('nom')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('nom');
+    myHeading.textContent = 'Bonjour, ' + storedName;
+  }
+
+  myButton.addEventListener('click', function() {
+    setUserName();
+  });
